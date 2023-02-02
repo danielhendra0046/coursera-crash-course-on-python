3aadbabc6665fa0e1b553d9fcae34c2fc36b5596
danielhendra0046 <danielhendra08@gmail.com>
2023-02-02 16:31:32 +0700

    add error issue

--- week_2.py
+++ week_2.py
@@ -35,14 +35,14 @@ print("The sum of both areas is: " + str(sum))
 # The sum of both areas is: 20.5
 
 
-def convert_seconds(second):
-	hours = seconds // 3600
-	minutes = (seconds - hours * 3600) // 60
-	remaining_seconds = seconds - hours * 3600 - minutes * 60
-	return hours, minutes, remaining_seconds
+#def convert_seconds(second):
+#	hours = seconds // 3600
+#	minutes = (seconds - hours * 3600) // 60
+#	remaining_seconds = seconds - hours * 3600 - minutes * 60
+#	return hours, minutes, remaining_seconds
 
-hours, minutes, seconds = convert_seconds(5000)
-print(hours, minutes, seconds)
+#hours, minutes, seconds = convert_seconds(5000)
+#print(hours, minutes, seconds)
 
 # Output:
 # 1 23 30
