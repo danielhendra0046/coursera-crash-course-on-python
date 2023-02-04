88565adf22b09108f82c157fa00d7fb3b40c70b6
danielhendra0046 <danielhendra08@gmail.com>
2023-02-04 09:56:24 +0700

    add new materials

--- week_3.py
+++ week_3.py
@@ -1,1 +1,23 @@
-abc
+# Website: https://www.coursera.org/learn/python-crash-course/home/week/3
+
+##################################################################################
+
+# SUBCHAPTER WHILE LOOPS
+
+# while loops: instruct your computer to continuously execute your code based on the value of he condition
+# initializing: to give an initial value to a variable
+
+x = 0
+while x < 5:
+  print("Not there yet, x=" + str(x))
+  x = x + 1
+print("x=" + str(x))
+
+# Output:
+# Not there yet, x=0
+# Not there yet, x=1
+# Not there yet, x=2
+# Not there yet, x=3
+# Not there yet, x=4
+# x=5
+
